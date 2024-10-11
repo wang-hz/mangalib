b39739c6aee5931f3ed5d1bc59a4e0c1039599f8
Wang HZ <36946658+wang-hz@users.noreply.github.com>
2024-10-12 01:00:00 +0800

    add columns in manga model; update manga updating logic

---
+++ prisma/migrations/20241011121303_add_columns_in_table_manga/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "manga" ADD COLUMN "artist" TEXT;
+ALTER TABLE "manga" ADD COLUMN "event" TEXT;
+ALTER TABLE "manga" ADD COLUMN "fullTitle" TEXT;
+ALTER TABLE "manga" ADD COLUMN "group" TEXT;
+ALTER TABLE "manga" ADD COLUMN "originalTitle" TEXT;
+ALTER TABLE "manga" ADD COLUMN "parody" TEXT;
+ALTER TABLE "manga" ADD COLUMN "tags" TEXT;
--- script.ts
+++ script.ts
@@ -6,6 +6,55 @@ import { v4 } from 'uuid';
 
 const prisma = new PrismaClient();
 
+const parseArtistAndGroup = (artist: string | undefined) => {
+  if (!artist) {
+    return [undefined, undefined];
+  }
+  const matches = artist.match(/(?<first>.*?(?=( \(|$)))( \((?<second>.*?)\))?/);
+  const first = matches?.groups?.first;
+  const second = matches?.groups?.second;
+  if (!matches || !second) {
+    return [artist, undefined];
+  }
+  return [second, first];
+};
+
+const reverseString = (s: string | undefined) => {
+  return s?.split('').reverse().join('');
+};
+
+const parseTags = (text: string | undefined) => {
+  if (!text) {
+    return '[]';
+  }
+  const tags = text.trim().split(' ').map(tag => tag.slice(1, -1));
+  return JSON.stringify(tags);
+};
+
+const parseTitle = (title: string | undefined) => {
+  if (!title) {
+    return [undefined, undefined];
+  }
+  const trimmedTitle = title.trim();
+  if (!trimmedTitle.includes('｜')) {
+    return [trimmedTitle, trimmedTitle];
+  }
+  return trimmedTitle.split('｜').map(s => s.trim());
+};
+
+const parseMangaInfo = (mangaPath: string) => {
+  const fullTitle = Path.basename(mangaPath, Path.extname(mangaPath));
+  const matches1 = fullTitle.match(/(\((?<event>.*?)\) )?(\[(?<artist>.*?)] )(?<rest>.*)/);
+  const event = matches1?.groups?.event;
+  const [artist, group] = parseArtistAndGroup(matches1?.groups?.artist);
+  const rest = matches1?.groups?.rest;
+  const matches2 = reverseString(rest)?.match(/(?<tags>].*\[ )?(\)(?<parody>.*)\( )?(?<title>.*)/);
+  const tags = parseTags(reverseString(matches2?.groups?.tags));
+  const parody = reverseString(matches2?.groups?.parody);
+  const [originalTitle, title] = parseTitle(reverseString(matches2?.groups?.title));
+  return { title, fullTitle, originalTitle, artist, group, parody, event, tags };
+};
+
 const getMangaPaths = (mangaDir: string) => {
   const mangaPaths: string[] = [];
   const stack = [mangaDir];
@@ -35,11 +84,23 @@ const createMangaIfNotExists = async (path: string) => {
         return;
       }
       const uuid = v4();
-      await prisma.manga.create({ data: { uuid, path, title: Path.parse(path).name } });
+      const info = parseMangaInfo(path);
+      await prisma.manga.create({ data: { uuid, path, ...info } });
     });
 };
 
 export const updateMangas = () => {
+  prisma.manga.findMany()
+    .then((mangas) => mangas.forEach(async (manga) => {
+      if (!fs.existsSync(manga.path)) {
+        await prisma.manga.delete({ where: { uuid: manga.uuid } });
+        return;
+      }
+      await prisma.manga.update({
+        where: { uuid: manga.uuid },
+        data: parseMangaInfo(manga.path)
+      });
+    }));
   getMangaPaths(MANGAS_DIR).forEach(async (mangaPath) => await createMangaIfNotExists(mangaPath));
 };
 
