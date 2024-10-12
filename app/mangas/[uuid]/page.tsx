247e92016d4c9f637e7105ddc66d824a2e2e48da
Wang HZ <36946658+wang-hz@users.noreply.github.com>
2024-10-13 01:02:42 +0800

    display cover on manga page

--- app/mangas/[uuid]/page.tsx
+++ app/mangas/[uuid]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { Box, Button, ButtonGroup, Stack } from "@mui/material";
+import { Button, ButtonGroup, Paper, Stack } from "@mui/material";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface MangaModel {
@@ -25,28 +26,48 @@ const getManga = async (uuid: string) => {
     .catch((error) => {
       console.error(error);
     });
-}
+};
+
+const getImages = async (mangaUuid: string) => {
+  return fetch(`/api/mangas/${mangaUuid}/images`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('failed to fetch images of manga');
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+};
 
 export default function Home({ params }: { params: { uuid: string } }) {
   const [manga, setManga] = useState<MangaModel | undefined>(undefined);
+  const [cover, setCover] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     getManga(params.uuid).then((data) => setManga(data));
+    getImages(params.uuid).then((data) => setCover(data.images[0]));
   }, [params.uuid]);
 
   return (
-    <Stack gap={1}>
-      {Object.entries(manga ?? {})
-        .filter(([key, value]) => !!value && key !== 'tags')
-        .map(([key, value], index) =>
-          <ButtonGroup key={index}>
-            <Button>{key}</Button>
-            <Button style={{ textTransform: 'none' }}>{value}</Button>
-          </ButtonGroup>)}
-      <Stack direction='row' gap={1}>
-        {manga?.tags.map((tag, index) =>
-          <Button key={index} variant='outlined' style={{ textTransform: 'none' }}>{tag}</Button>
-        )}
+    <Stack direction='row' gap={1}>
+      <Paper style={{ position: 'relative', width: '300px', height: '400px' }}>
+        {cover && <Image src={cover} alt='cover' fill objectFit='contain'/>}
+      </Paper>
+      <Stack gap={1}>
+        {Object.entries(manga ?? {})
+          .filter(([key, value]) => !!value && key !== 'tags')
+          .map(([key, value], index) =>
+            <ButtonGroup key={index}>
+              <Button>{key}</Button>
+              <Button style={{ textTransform: 'none' }}>{value}</Button>
+            </ButtonGroup>)}
+        <Stack direction='row' gap={1}>
+          {manga?.tags.map((tag, index) =>
+            <Button key={index} variant='outlined' style={{ textTransform: 'none' }}>{tag}</Button>
+          )}
+        </Stack>
       </Stack>
     </Stack>
   );
