ee888367672bcd69a6fff70965bc4e3443715a5c
Wang HZ <36946658+wang-hz@users.noreply.github.com>
2024-10-28 21:27:57 +0800

    check if file exists before remove it

--- app/script.ts
+++ app/script.ts
@@ -165,7 +165,10 @@ export const findManga = async (uuid: string) => {
 
 export const deleteImages = async (mangaUuid: string) => {
   const images = await prisma.image.findMany({ where: { mangaUuid } });
-  images.map((image) => image.filename).forEach((filename) => fs.unlinkSync(Path.join(CACHE_DIR, filename)));
+  images.map((image) => image.filename)
+    .map((filename) => Path.join(CACHE_DIR, filename))
+    .filter((image_path) => fs.existsSync(image_path))
+    .forEach((image_path) => fs.unlinkSync(image_path));
   return prisma.image.deleteMany({ where: { mangaUuid } });
 };
 
